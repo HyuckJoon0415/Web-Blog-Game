188eaa74ac9b38b3d61a556eabbe61d73e9849f1
HyuckJoon0415 <lmaodct1999@gmail.com>
2023-11-20 11:14:09 +0900

    세상에 고통을

--- js/slideShow.js
+++ js/slideShow.js
@@ -6,15 +6,19 @@ let cnt = 0;
 nextBt.addEventListener("click", function () {
     if (cnt == 0) slideShow.style.transform = "translate(-100vw)";
     else if (cnt == 1) slideShow.style.transform = "translate(-200vw)";
+    else if (cnt == 2) slideShow.style.transform = "translate(-300vw)";
+    else if (cnt == 3) slideShow.style.transform = "translate(-400vw)";
     else slideShow.style.transform = "translate(0vw)";
     cnt++;
-    if (cnt > 2) cnt = 0;
+    if (cnt > 4) cnt = 0;
 });
 
 prevBt.addEventListener("click", function () {
-    if (cnt == 0) slideShow.style.transform = "translate(-200vw)";
-    else if (cnt == 1) slideShow.style.transform = "translate(-100vw)";
+    if (cnt == 0) slideShow.style.transform = "translate(-400vw)";
+    else if (cnt == 1) slideShow.style.transform = "translate(-300vw)";
+    else if (cnt == 2) slideShow.style.transform = "translate(-200vw)";
+    else if (cnt == 3) slideShow.style.transform = "translate(-100vw)";
     else slideShow.style.transform = "translate(0vw)";
     cnt++;
-    if (cnt > 2) cnt = 0;
+    if (cnt > 4) cnt = 0;
 });
